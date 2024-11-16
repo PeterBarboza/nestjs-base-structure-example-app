a74395c75f0188d62f7d94414187355688fb85f3
PeterBarboza <pedro.barboza.dev@gmail.com>
2024-11-16 16:29:31 -0300

    feat: updating AllExceptionsFilter

--- src/common/exceptions/exceptions.filter.ts
+++ src/common/exceptions/exceptions.filter.ts
@@ -3,26 +3,47 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Catch()
+interface JsonResponse {
+  status: number;
+  message: string | string[];
+  timestamp: string;
+  path: string;
+}
+type ExceptionResponse =
+  | {
+      message: string | string[];
+    }
+  | string;
+
+@Catch(HttpException)
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: HttpException | Error, host: ArgumentsHost) {
+  catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    let status = 500;
-    if (exception instanceof HttpException) {
-      status = exception.getStatus();
-    }
-
-    response.status(status).json({
-      statusCode: status,
-      message: exception.message,
+    const responseData: JsonResponse = {
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal Server Error',
       timestamp: new Date().toISOString(),
       path: request.url,
-    });
+    };
+
+    if (exception instanceof HttpException) {
+      const response = exception.getResponse() as ExceptionResponse;
+      responseData.status = exception.getStatus();
+
+      if (typeof response === 'string') {
+        responseData.message = response;
+      } else {
+        responseData.message = response.message;
+      }
+    }
+
+    response.status(responseData.status).json(responseData);
   }
 }
