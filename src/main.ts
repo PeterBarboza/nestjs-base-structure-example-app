0205ebb5c0e67d01b720f5a35e1cb06840f4a7d4
PeterBarboza <pedro.barboza.dev@gmail.com>
2024-11-20 14:38:41 -0300

    feat: Updating ValidationPipe options

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AllExceptionsFilter } from './common/exceptions/exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT ?? 3000);
